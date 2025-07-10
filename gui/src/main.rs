73037eae717dee2ad2620c01aedc4a5914bca786
MadsWedendahlKruse <madswk2@gmail.com>
2025-07-10 21:05:03 +0200

    Enhance GUI to display character details and stats in the main window

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -3,7 +3,12 @@ use std::{num::NonZeroU32, time::Instant};
 mod utils;
 
 use glutin::surface::GlSurface;
-use imgui::Condition;
+use imgui::{Condition, TreeNodeFlags};
+use nat20_rs::{
+    stats::{ability::Ability, d20_check::RollMode, proficiency::Proficiency, skill::Skill},
+    test_utils::fixtures,
+};
+use strum::IntoEnumIterator;
 use utils::Triangler;
 
 fn main() {
@@ -16,6 +21,8 @@ fn main() {
     let tri_renderer = Triangler::new(ig_renderer.gl_context(), "#version 330");
 
     let mut last_frame = Instant::now();
+
+    let character = fixtures::creatures::heroes::fighter();
 
     #[allow(deprecated)]
     event_loop
@@ -48,18 +55,103 @@ fn main() {
                     let ui = imgui_context.frame();
                     ui.show_demo_window(&mut true);
 
-                    ui.window("Hello world")
-                        .size([300.0, 100.0], Condition::FirstUseEver)
+                    ui.window(character.name())
+                        .size([300.0, 1200.0], Condition::FirstUseEver)
                         .build(|| {
-                            ui.text("Hello world!");
-                            ui.text("こんにちは世界！");
-                            ui.text("This...is...imgui-rs!");
-                            ui.separator();
-                            let mouse_pos = ui.io().mouse_pos;
-                            ui.text(format!(
-                                "Mouse Position: ({:.1},{:.1})",
-                                mouse_pos[0], mouse_pos[1]
-                            ));
+                            ui.text(format!("ID: {}", character.id()));
+                            ui.text(format!("Level: {}", character.total_level()));
+                            ui.text(format!("HP: {}/{}", character.hp(), character.max_hp()));
+
+                            if ui.collapsing_header("Classes", TreeNodeFlags::DEFAULT_OPEN) {
+                                for (class_name, level) in character.classes() {
+                                    if let Some(subclass_name) = character.subclass(class_name) {
+                                        ui.text(format!(
+                                            "Level {} {} {}",
+                                            level, subclass_name.name, class_name
+                                        ));
+                                    } else {
+                                        ui.text(format!("Level {} {}", level, class_name));
+                                    }
+                                }
+                            }
+
+                            if ui.collapsing_header("Ability Scores", TreeNodeFlags::DEFAULT_OPEN) {
+                                for (_, ability_score) in character.ability_scores().scores.iter() {
+                                    ui.text(format!("{}", ability_score));
+                                }
+                            }
+
+                            if ui.collapsing_header("Skills", TreeNodeFlags::DEFAULT_OPEN) {
+                                for skill in Skill::iter() {
+                                    let stats = character.skills().get(skill);
+                                    if stats.modifiers().is_empty()
+                                        && stats.advantage_tracker().roll_mode() == RollMode::Normal
+                                        && *stats.proficiency() == Proficiency::None
+                                    {
+                                        continue; // Skip skills with no modifiers
+                                    }
+                                    ui.text(format!(
+                                        "{}: {}",
+                                        skill,
+                                        stats.format_bonus(character.proficiency_bonus())
+                                    ));
+                                }
+                            }
+
+                            if ui.collapsing_header("Saving Throws", TreeNodeFlags::DEFAULT_OPEN) {
+                                for ability in Ability::iter() {
+                                    let stats = character.saving_throws().get(ability);
+                                    if stats.modifiers().is_empty()
+                                        && stats.advantage_tracker().roll_mode() == RollMode::Normal
+                                        && *stats.proficiency() == Proficiency::None
+                                    {
+                                        continue; // Skip saving throws with no modifiers
+                                    }
+                                    ui.text(format!(
+                                        "{}: {}",
+                                        ability,
+                                        stats.format_bonus(character.proficiency_bonus())
+                                    ));
+                                }
+                            }
+
+                            if ui.collapsing_header("Resources", TreeNodeFlags::DEFAULT_OPEN) {
+                                for (resource_id, resource) in character.resources().iter() {
+                                    ui.text(format!(
+                                        "{}: {}/{}",
+                                        resource_id,
+                                        resource.current_uses(),
+                                        resource.max_uses()
+                                    ));
+                                }
+                            }
+
+                            if ui.collapsing_header("Spell slots", TreeNodeFlags::DEFAULT_OPEN) {
+                                if character.spellbook().spell_slots().is_empty() {
+                                    ui.text("No spell slots available\n");
+                                }
+                                for (level, (current_slots, max_slots)) in
+                                    character.spellbook().spell_slots().iter()
+                                {
+                                    ui.text(format!(
+                                        "Level {}: ({}/{})\n",
+                                        level, current_slots, max_slots
+                                    ));
+                                }
+                            }
+
+                            if ui.collapsing_header("Effects", TreeNodeFlags::DEFAULT_OPEN) {
+                                for effect in character.effects() {
+                                    ui.text(format!("{} ({})", effect.id(), effect.duration()));
+                                }
+                            }
+
+                            // ui.separator();
+                            // let mouse_pos = ui.io().mouse_pos;
+                            // ui.text(format!(
+                            //     "Mouse Position: ({:.1},{:.1})",
+                            //     mouse_pos[0], mouse_pos[1]
+                            // ));
                         });
 
                     winit_platform.prepare_render(ui, &window);
